29c02b07fb07d410d0e85629aaac669f7d4c8b70
alex <x103103@gmail.com>
2015-05-16 10:09:52 +0300

    extend Reader test

--- spec/lib/reader_spec.rb
+++ spec/lib/reader_spec.rb
@@ -1,14 +1,33 @@
 require 'spec_helper'
 
 describe Library::Reader do
+  let(:reader) do
+    Library::Reader.new({
+                         name:'q',
+                         email:'w',
+                         city:'e',
+                         street:'r',
+                         house:'t'
+                     })
+  end
 
   it 'should have name' do
-    reader = Library::Reader.new(name:'s')
-    expect(reader.name).to eq('s')
+    expect(reader.name).to eq('q')
   end
 
   it 'should have email' do
-    reader = Library::Reader.new(email:'s')
-    expect(reader.email).to eq('s')
+    expect(reader.email).to eq('w')
+  end
+
+  it 'should have city' do
+    expect(reader.city).to eq('e')
+  end
+
+  it 'should have street' do
+    expect(reader.street).to eq('r')
+  end
+
+  it 'should have house' do
+    expect(reader.house).to eq('t')
   end
 end
